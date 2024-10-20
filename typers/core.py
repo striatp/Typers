cc33957c014d38bf08305e38b5fd05c89abbf4ac
Sébastien <sebastien.works@outlook.com>
2024-10-20 18:50:25 +0200

    Update core.py

--- typers/core.py
+++ typers/core.py
@@ -43,3 +43,23 @@ class Valid:
             raise ValidateError("The 'name' argument must be a string : preferably the name of the relative function.")
         if not isinstance(value, int) or value > 0:
             raise ValueError(f"The '{name}' argument must be a negative integer excluding 0.")
+    @staticmethod
+    def _float(value, name: str = None):
+        if name is None:
+            raise ValidateError("The 'name' argument must be a string.")
+        if not isinstance(value, float):
+            raise ValueError(f"The '{name}' argument must be a float.")
+
+    @staticmethod
+    def _positive_float(value, name: str = None):
+        if name is None:
+            raise ValidateError("The 'name' argument must be a string.")
+        if not isinstance(value, float) or value <= 0:
+            raise ValueError(f"The '{name}' argument must be a positive float.")
+
+    @staticmethod
+    def _negative_float(value, name: str = None):
+        if name is None:
+            raise ValidateError("The 'name' argument must be a string.")
+        if not isinstance(value, float) or value >= 0:
+            raise ValueError(f"The '{name}' argument must be a negative float.")
